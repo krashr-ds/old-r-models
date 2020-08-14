7b1a87a4e694631e7baebb2e1619333292062995
krashr-ds <kprasku@tutanota.com>
2020-08-14 19:06:05 -0400

    Update cc_exploratory1.r

--- cc_exploratory1.r
+++ cc_exploratory1.r
@@ -97,11 +97,15 @@ cc <- cc %>% mutate(TOTAL_AVE_NUM_SCRIPTS_PER_BENE = as.numeric(as.character(AVE
 
 cc <- cc %>% mutate(TOTAL_AVE_COSTS = TOTAL_AVE_REIMB_PA + TOTAL_AVE_REIMB_PB)
 
-# FLATTENED, CATEGORIZED DATA: 242033 rows and 68 cols #
+# FLATTENED, CATEGORIZED DATA: 242033 rows and 67 cols #
 ########################################################
 
 # CONSTANT ASSIGNMENT
+
+# NOTE: THIS #, FROM THE CC PUF PDF, IS NOT IDENTICAL TO THE # IN THE CHARTBOOK REFERENCE
 TOTAL_2010_BENEFICIARIES = 50088835
+
+# FROM THE CMS WEBSITE: TOTAL A & B BENEFICIARIES FOR 2010
 TOTAL_2010_AB_SPEND = 338000000000
 
 # REMOVE NA VALUES FOR CALCULATIONS ONLY
@@ -112,5 +116,23 @@ cc_1[is.na(cc_1)] <- 0
 TOTAL_2010_CC_AB_SPEND = 6059858244
 PROP_2010_CC_AB_SPEND = TOTAL_2010_CC_AB_SPEND / TOTAL_2010_AB_SPEND
 
+# CC PREVALENCE
+# PREVALENCE RELIES ON TIME PERIOD; FOR ACCURACY WE MUST USE THE EQ 12 PORTION OF THE SAMPLE
+# WE MUST ALSO GROUP BY CONDITION BECAUSE WHERE # OF CONDITIONS > 1, SUMMARY COUNTS ARE NOT ACCURATE
+cc_prev_groups <- cc_1 %>% group_by(CONDITION) %>% filter(HAS_CONDITION == 1 & as.numeric(as.character(BENE_COUNT_PA_EQ_12)) > 0 & CC_2_OR_MORE == "1")
+cc_prev_sum <- cc_prev_groups %>% group_by(CONDITION) %>% summarize(TWO_OR_MORE = n(), SUM_BENE_A_EQ_12 = sum(as.numeric(as.character(BENE_COUNT_PA_EQ_12))), PREV = (TWO_OR_MORE * 100) / SUM_BENE_A_EQ_12) %>% arrange(desc(PREV))
+cc_prev_sum2 <- cc_prev_sum %>% summarize(MEAN_PREVALENCE = mean(PREV), MEDIAN_PREVALENCE = median(PREV), RANGE1_PREVALENCE = min(range(PREV)), RANGE2_PREVALENCE = max(range(PREV)))
+  
 # SUMMARIZE
 cc_sum <- cc_1 %>% group_by(CONDITION) %>% filter(HAS_CONDITION == 1) %>% summarize(ROWCOUNT = n(), SUM_BENE_A = sum(TOTAL_BENE_A), SUM_BENE_B = sum(TOTAL_BENE_B), SUM_BENE_C = sum(TOTAL_BENE_C), SUM_BENE_D = sum(TOTAL_BENE_D), SUM_AVE_A_COST = sum(TOTAL_AVE_REIMB_PA), SUM_AVE_B_COST = sum(TOTAL_AVE_REIMB_PB), SUM_AVE_DRUG_COST = sum(TOTAL_AVE_DRUG_COST_PER_BENE), SUM_AVE_ALL_AB = sum(TOTAL_AVE_COSTS), PROP_AB_COST = (SUM_AVE_ALL_AB / TOTAL_2010_AB_SPEND), PROP_CC_AB_COST = (SUM_AVE_ALL_AB / TOTAL_2010_CC_AB_SPEND), MEAN_IP_ADM = mean(TOTAL_IP_ADM_PER_BENE), MEAN_SNF_DAYS = mean(TOTAL_AVE_NUM_SNF_DAYS_PER_BENE), MEAN_SCRIPTS = mean(TOTAL_AVE_NUM_SCRIPTS_PER_BENE))
+
+# ARRANGE
+cc_sum2a <- cc_sum %>% group_by(CONDITION) %>% arrange(desc(SUM_BENE_A))
+cc_sum2 <- cc_sum %>% group_by(CONDITION) %>% arrange(desc(SUM_AVE_ALL_AB))
+cc_sum3 <- cc_sum %>% group_by(CONDITION) %>% arrange(desc(SUM_AVE_DRUG_COST))
+cc_sum4 <- cc_sum %>% group_by(CONDITION) %>% arrange(desc(MEAN_IP_ADM))
+
+# PART A & B SNF COSTS ~ TOP 3 (SNF DAYS)
+cc_snf <- cc_1 %>% group_by(CONDITION) %>% filter(HAS_CONDITION == 1 & as.numeric(as.character(TOTAL_AVE_NUM_SNF_DAYS_PER_BENE)) > 0)
+cc_snf_sum <- cc_snf %>% group_by(CONDITION) %>% summarize(ROWCOUNT = n(), TOTAL_SNF_SPEND = sum(TOTAL_AVE_REIMB_PA_SNF), TOTAL_SNF_DAYS = sum(TOTAL_AVE_NUM_SNF_DAYS_PER_BENE)) %>% arrange(desc(TOTAL_SNF_SPEND))
+
